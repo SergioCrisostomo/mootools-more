1948121e81d6ee46899710452abdd467614837e1
Sergio Crisostomo <sergiosbox@gmail.com>
2014-04-18 14:54:12 +0200

    add Drag specs mock
    http://jsfiddle.net/Z58J3/ - with html/css/js
    http://jsfiddle.net/nFbbF/ - just JS

---
+++ Specs/Drag/Drag.js
@@ -0,0 +1,96 @@
+/*
+---
+name: Drag
+requires: [Core/Events, Core/Options, Core/Element.Event, Core/Element.Style, Core/Element.Dimensions, MooTools.More]
+provides: [Drag]
+...
+*/
+(function(){
+    new Element('p', {
+        'html': 'MooTools Drag test. This paragraph is needed for the test!'
+    }).inject($(document.body));
+
+    var parentContainer = new Element('div', {
+        id: 'container',
+        styles: {
+            width: '200px',
+            height: '200px',
+            background: 'red',
+            margin: '20px',
+        }
+    }).inject($(document.body));
+
+    new Element('div', {
+        id: 'drag',
+        styles: {
+            width: '31px',
+            height: '31px',
+            background: 'blue',
+            position: 'relative'
+        }
+    }).inject(parentContainer);
+
+    var dragSize = $('drag').getSize();
+    var containerSize = $('container').getSize();
+    var windowSize = window.getSize();
+    var corners = [{
+        x: 0,
+        y: 0
+    }, {
+        x: 0,
+        y: windowSize.y
+    }, {
+        x: windowSize.x,
+        y: 0
+    }, {
+        x: windowSize.x,
+        y: windowSize.y
+    }];
+
+    function borderPolice(container, drag) {
+        var clean = true;
+        if (drag.x < container.x || (drag.x + dragSize.x) > container.x + containerSize.x) clean = false;
+        if (drag.y < container.y || (drag.y + dragSize.y) > container.y + containerSize) clean = false;
+        return clean;
+    }
+    function getCenter(pos){
+        return {
+            pageX: pos.x,
+            pageY: pos.y
+        };
+    }
+
+    describe('Drag', function(){
+
+        it('should drag the box inside the parent without crossing the border', function(){
+            var borderFlag = true;
+            var theDrag = new Drag.Move('drag', {
+                container: 'container',
+                includeMargins: false,
+                onDrag: function () {
+                    var containerPosition = this.container.getPosition();
+                    var dragPosition = this.element.getPosition();
+                    // the veridict
+                    if (!borderPolice(containerPosition, dragPosition)) borderFlag = false;
+                }
+            });
+            var cI = 0;
+            var walkThru = setInterval(function () {
+                var nextCorner = corners[cI];
+                Syn.drag({
+                    from: getCenter($('drag').getPosition()),
+                    to: getCenter(nextCorner)
+                }, 'drag');
+                cI++;
+                if (cI == corners.length) clearInterval(walkThru);
+            }, 100);
+
+            waits(800);
+
+            runs(function(){
+                expect(borderFlag).toBeTruthy();
+            });
+
+        });
+    });
+})();
