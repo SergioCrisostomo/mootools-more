1afe51d1e04097f8c458c15295c74f2e454d9e63
Sergio Crisostomo <sergiosbox@gmail.com>
2014-11-16 19:30:17 +0100

    Update Scroller.js

--- Specs/Interface/Scroller.js
+++ Specs/Interface/Scroller.js
@@ -6,41 +6,48 @@ provides: [Scroller.Tests]
 ...
 */
 describe('Scroller', function(){
-	var inner = new Element('div', {
-		id: 'myScroll',
-		styles: {
-			width: 300,
-			height: 200,
-			overflow: 'scroll'
-		}
-	});
-	inner.inject($(document.body));
-	var wrapper = new Element('div', {
-		styles: {
-			width: 600,
-			height: 400
-		}
-	}).inject(inner);
 
-	var myScroller = new Scroller('myScroll', {
-		area: Math.round(window.getWidth() / 10)
-	});
+    var inner, wrapper, myScroller, body = $(document.body);
 
-	it('should initialize', function(){
-		expect(myScroller.element).toEqual(inner);
-	});
-	it('should be error free', function(){
-		var error;
-		try{
-			myScroller.scroll();
-		}catch(e){
-			error = e;
-		}
-		expect(error).toBe(undefined);
-	});
+    beforeEach(function(){
+        inner = new Element('div', {
+            id: 'myScroll',
+            styles: {
+                width: 300,
+                height: 200,
+                overflow: 'scroll'
+            }
+        });
+        inner.inject(body);
+        wrapper = new Element('div', {
+            styles: {
+                width: 600,
+                height: 400
+            }
+        }).inject(inner);
 
-	inner.destroy();
-	wrapper.destroy();
-	myScroller = null;
+        myScroller = new Scroller('myScroll', {
+            area: Math.round(window.getWidth() / 10)
+        });
+    });
+
+    afterEach(function(){
+        inner.destroy();
+        wrapper.destroy();
+        myScroller = null;
+    });
+
+    it('should initialize', function(){
+        expect(myScroller.element).toEqual(inner);
+    });
+    it('should be error free', function(){
+        var error;
+        try{
+            myScroller.scroll();
+        }catch(e){
+            error = e;
+        }
+        expect(error).toBe(undefined);
+    });
 
 });
